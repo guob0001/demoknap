b6b0c3d62e0970a9b2db323e4384ad74a224ad5e
Guobin Chen <guobinchen@Guobins-MacBook-Pro.local>
2021-06-29 23:11:38 +0200

    Initial commit

---
+++ src/main/java/com/example/demoknap/DemoknapApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoknap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoknapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoknapApplication.class, args);
+    }
+
+}
